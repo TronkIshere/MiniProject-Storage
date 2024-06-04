3ae9cce0b076104d3fbce41a75efb97c17f07f6b
TronkIsHere <137505475+TronkIshere@users.noreply.github.com>
2024-06-04 22:56:33 +0700

    A mini web chat make with Reactjs and spring boot.(re-up)

---
+++ miniWebChat/chatServer/src/main/java/com/example/chatServerMiniProject/model/Status.java
@@ -0,0 +1,7 @@
+package com.example.chatServerMiniProject.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
---
+++ miniWebChat/chatServer/src/test/java/com/example/chatServerMiniProject/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatServerMiniProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
