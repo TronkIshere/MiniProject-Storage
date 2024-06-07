e2fe5af7e9f0a59b6a494474e9a2a1cec060be82
TronkIsHere <137505475+TronkIshere@users.noreply.github.com>
2024-06-07 14:15:40 +0700

    Completed video page.

---
+++ youtube-clone/src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './Recommended.css'
+import thumbnail from '../../assets/VuongTuLoren.jpg'
+
+const Recommended = () => {
+  return (
+    <div className='recommended'>
+        <div className="side-video-list">
+            <img src={thumbnail} alt=''/>
+            <div className="video-info">
+                <h4>Best channel to learn farming that help you to win dark souls 3</h4>
+                <p>Mr.DoingOut</p>
+                <p>20m views &bull; 1 day ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail} alt=''/>
+            <div className="video-info">
+                <h4>Best channel to learn farming that help you to win dark souls 3</h4>
+                <p>Mr.DoingOut</p>
+                <p>20m views &bull; 1 day ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail} alt=''/>
+            <div className="video-info">
+                <h4>Best channel to learn farming that help you to win dark souls 3</h4>
+                <p>Mr.DoingOut</p>
+                <p>20m views &bull; 1 day ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail} alt=''/>
+            <div className="video-info">
+                <h4>Best channel to learn farming that help you to win dark souls 3</h4>
+                <p>Mr.DoingOut</p>
+                <p>20m views &bull; 1 day ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail} alt=''/>
+            <div className="video-info">
+                <h4>Best channel to learn farming that help you to win dark souls 3</h4>
+                <p>Mr.DoingOut</p>
+                <p>20m views &bull; 1 day ago</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src={thumbnail} alt=''/>
+            <div className="video-info">
+                <h4>Best channel to learn farming that help you to win dark souls 3</h4>
+                <p>Mr.DoingOut</p>
+                <p>20m views &bull; 1 day ago</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Recommended
