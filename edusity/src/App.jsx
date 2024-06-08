ba2ccc34709685c2b7219fd1c630d3342d2d4bad
TronkIsHere <137505475+TronkIshere@users.noreply.github.com>
2024-06-08 17:39:10 +0700

    Add Contact, Footer Components into Edusity project, complete the overall look.

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -6,6 +6,8 @@ import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
         <Title subTitle='TESTIMONIALS' title='What Student Says'/>
         <Testimonials/>
         <Title subTitle='Contact Us' title='Get in touch'/>
+        <Contact/>
+        <Footer/>
       </div>
     </div>
   )
---
+++ edusity/src/Components/Contact/Contact.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import "./Contact.css";
+import msg_icon from "../../assets/msg-icon.png";
+import mail_icon from "../../assets/mail-icon.png";
+import phone_icon from "../../assets/phone-icon.png";
+import location_icon from "../../assets/local-icon.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
+const Contact = () => {
+
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "69af4c0d-a9b5-44c4-8022-5c5fcda9f9fb");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+
+  return (
+    <div className="contact">
+      <div className="contact-col">
+        <h3>
+          Send us a message <img src={msg_icon} alt="" />
+        </h3>
+        <p>
+          Feel free to reach out through contact form or find our contact
+          information below. Your feedback, questions, and suggestions are
+          important to us as we strive to provide exceptional service to our
+          university community.
+        </p>
+        <ul>
+          <li>
+            <img src={mail_icon} alt="" />
+            nguyenhuutrong11133@gmail.com
+          </li>
+          <li>
+            <img src={phone_icon} alt="" />
+            035-925-6696
+          </li>
+          <li>
+            <img src={location_icon} alt="" />
+            77 Massachusetts Ave, Cambridge
+            <br /> MA 02139, United States
+          </li>
+        </ul>
+      </div>
+      <div className="contact-col">
+        <form onClick={onSubmit}>
+          <label>Your name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your mobile number"
+            required
+          />
+          <label>Write your message here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit now
+            <FontAwesomeIcon icon={faArrowRight} className="icon-in-button"/>
+          </button>
+        </form>
+        <span>{result}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
