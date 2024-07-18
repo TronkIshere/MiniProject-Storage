90ce83ca9be55e41cee2cc5662e888a2f0b6b1fa
TronkIsHere <nguyenhuutrong11133@gmail.com>
2024-07-18 17:31:59 +0700

    Add Bar, Line, Pie and Geography Charts with Nivo Charts into Admin-dashBroad project.

--- Admin-dashBoard/src/App.jsx
+++ Admin-dashBoard/src/App.jsx
@@ -8,13 +8,13 @@ import { Route, Routes } from "react-router-dom";
 import Team from "./scenes/team";
 import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
-// import Bar from "./scenes/bar";
+import Bar from "./scenes/bar";
 import Form from "./scenes/form";
 import { Calendar } from "@fullcalendar/core/index.js";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
 import FAQ from "./scenes/faq";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -35,11 +35,10 @@ function App() {
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<FAQ />} />
-              {/* <Route path="/bar" element={<Bar />} />
+              <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              
-              <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/geography" element={<Geography />} /> 
             </Routes>
           </main>
         </div>
